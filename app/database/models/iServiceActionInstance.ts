70f7c6111d3989fd52f09ac8037ec89bbc7d2318
Emre Colakoglu <emrinho@gmail.com>
2018-01-28 14:17:49 +0300

    work

--- app/database/models/iServiceActionInstance.ts
+++ app/database/models/iServiceActionInstance.ts
@@ -3,6 +3,7 @@ import { IBase, IServiceAction, IUser, IApplet } from "./index";
 export interface IServiceActionInstance {
   serviceAction: IServiceAction;
   applet: IApplet;
+  /** action instance gecici bellek */
   payload: any;
 }
 
--- panel/pages/applets/create/createAppletsController.js
+++ panel/pages/applets/create/createAppletsController.js
@@ -1,37 +1,70 @@
 angular.module('pablo')
   .controller('createAppletsController', ["$scope", "$state", "restService", function ($scope, $state, restService) {
     var self = this;
+    self.applet = {};
     self.services = [];
     self.selectedService = {};
-    self.step = 1;
+    self.selectedAction = {};
+    self.step = 0;
 
-    self.getServices = function(){
-      restService.get("services", null).then(function(services){
+    self.saveApplet = function () {
+      self.step = 1;
+    };
+
+    self.getServices = function () {
+      restService.get("services", null).then(function (services) {
         self.services = services;
       });
     };
 
-    self.selectService = function(service){
+    self.selectService = function (service) {
       self.selectedService = service;
-      if(self.selectedService.requireAuth){
+      if (self.selectedService.requireAuth) {
         self.step = 2;
       }
-      else{
+      else {
         self.step = 3;
         self.getActions(self.selectedService._id);
       }
-      
     };
 
-    self.saveServiceUserPassCredentials = function(){
-      self.step = 3;
-      self.getActions(self.selectedService._id);
-    }
-
-    self.getActions = function(serviceId){
-      restService.get("services/" + serviceId + "/actions", null).then(function(actions){
+    self.getActions = function (serviceId) {
+      restService.get("services/" + serviceId + "/actions", null).then(function (actions) {
         self.actions = actions;
       });
+    };
+
+    self.saveServiceUserPassCredentials = function () {
+      self.step = 3;
+      self.getActions(self.selectedService._id);
+    };
+
+    self.selectAction = function (action) {
+      self.selectedAction = action;
+      if (self.selectedAction.inputs && self.selectedAction.inputs.length > 0) {
+        self.step = 4;
+      }
+      else {
+        self.saveActionToApplet(self.convertActionToInstance(action));
+      }
+    };
+
+    self.saveInputs = function(){
+      self.saveActionToApplet(self.convertActionToInstance(action));
+    };
+
+    self.convertActionToInstance = function(action){
+      var payload = {};
+      
+      return {
+        serviceAction = action._id,
+        payload: payload
+      }
+    };
+
+    self.saveActionToApplet = function(action){
+      self.applet.inputs = self.applet.inputs || [];
+      self.applet.inputs.push(action);
     };
 
     self.init = function () {
